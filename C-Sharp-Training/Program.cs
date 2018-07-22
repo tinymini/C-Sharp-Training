3ad78bb0264961218a8dbb771f285124faf0c4a4
SiHyeong Kim <aberial@hotmail.com>
2018-07-22 19:16:40 +0900

    initial commit

---
+++ C-Sharp-Training/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
